23ed6effce3dc505e443bf2376fd778fb6f8b98a
backtofayton <yucelolcay@gmail.com>
2022-01-30 15:15:05 +0200

    First

---
+++ narcissistic_number.py
@@ -0,0 +1,30 @@
+# A Narcissistic Number is a positive number which is the sum of its own digits, each raised to the power of the number of digits in a given base. In this Kata, we will restrict ourselves to decimal (base 10).
+
+def power(x, y):
+    result = x
+    while y > 1:
+        result = result*x
+        y = y-1
+    if y == 0:
+        return 1
+    if y < 0:
+        return('please enter positive integer')
+    return result
+
+
+def narcissistic(n):
+    sum = 0
+    for i in list(str(n)):
+        sum = sum+power(int(i), len(str(n)))
+    if sum == n:
+        return True
+    else:
+        return False
+
+
+print(power(2, 3))
+print(power(1, 0))
+print(power(1, -2))
+
+print(narcissistic(2345))
+print(narcissistic(153))
