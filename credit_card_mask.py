857a17e5dbc9296f55d6ab6cd68284091cb0fb76
backtofayton <yucelolcay@gmail.com>
2022-01-30 17:26:08 +0200

    New algo

--- credit_card_mask.py
+++ credit_card_mask.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 # Usually when you buy something, you're asked whether your credit card number, phone number or answer to your most secret question is still correct. However, since someone could look over your shoulder, you don't want that shown on your screen. Instead, we mask it.
 # Your task is to write a function maskify, which changes all but the last four characters into '#'.
 
@@ -13,19 +12,3 @@ def maskify(cc):
 
 
 print(maskify('sdflasdfjaslkdgj'))
-=======
-# Usually when you buy something, you're asked whether your credit card number, phone number or answer to your most secret question is still correct. However, since someone could look over your shoulder, you don't want that shown on your screen. Instead, we mask it.
-# Your task is to write a function maskify, which changes all but the last four characters into '#'.
-
-# return masked string
-def maskify(cc):
-    dLen = len(cc)
-    strnew = ''
-    for l in cc[:dLen-4-1]:
-        strnew += '#'
-    return strnew+cc[-4:]
-    pass
-
-
-print(maskify('sdflasdfjaslkdgj'))
->>>>>>> master
--- decode_the_morse_code.py
+++ decode_the_morse_code.py
@@ -1,49 +1,21 @@
-<<<<<<< HEAD
 # The Morse code encodes every character as a sequence of "dots" and "dashes". For example, the letter A is coded as ·−, letter Q is coded as −−·−, and digit 1 is coded as ·−−−−. The Morse code is case-insensitive, traditionally capital letters are used. When the message is written in Morse code, a single space is used to separate the character codes and 3 spaces are used to separate words. For example, the message HEY JUDE in Morse code is ···· · −·−−   ·−−− ··− −·· ·.
 # NOTE: Extra spaces before or after the code have no meaning and should be ignored.
 # In addition to letters, digits and some punctuation, there are some special service codes, the most notorious of those is the international distress signal SOS (that was first issued by Titanic), that is coded as ···−−−···. These special codes are treated as single special characters, and usually are transmitted as separate words.
 # Your task is to implement a function that would take the morse code as input and return a decoded human-readable string.
 
 def decodeMorse(morse_code):
-    words=morse_code.strip().split('   ')
-    words_asci=[]
+    words = morse_code.strip().split('   ')
+    words_asci = []
     for word in words:
-        letters=''
+        letters = ''
         for letter in word.strip().split(' '):
-            letter_asci=MORSE_CODE[letter]
-            letters=letters+letter_asci
+            letter_asci = MORSE_CODE[letter]
+            letters = letters+letter_asci
         words_asci.append(letters)
     return ' '.join(words_asci)
 
-print(decodeMorse('   .   . '))
-        
-        
-        
-    # ToDo: Accept dots, dashes and spaces, return human-readable message
-    #return morse_code.replace('.', MORSE_CODE['.']).replace('-', MORSE_CODE['-']).replace(' ', '')
-
-=======
-# The Morse code encodes every character as a sequence of "dots" and "dashes". For example, the letter A is coded as ·−, letter Q is coded as −−·−, and digit 1 is coded as ·−−−−. The Morse code is case-insensitive, traditionally capital letters are used. When the message is written in Morse code, a single space is used to separate the character codes and 3 spaces are used to separate words. For example, the message HEY JUDE in Morse code is ···· · −·−−   ·−−− ··− −·· ·.
-# NOTE: Extra spaces before or after the code have no meaning and should be ignored.
-# In addition to letters, digits and some punctuation, there are some special service codes, the most notorious of those is the international distress signal SOS (that was first issued by Titanic), that is coded as ···−−−···. These special codes are treated as single special characters, and usually are transmitted as separate words.
-# Your task is to implement a function that would take the morse code as input and return a decoded human-readable string.
-
-def decodeMorse(morse_code):
-    words=morse_code.strip().split('   ')
-    words_asci=[]
-    for word in words:
-        letters=''
-        for letter in word.strip().split(' '):
-            letter_asci=MORSE_CODE[letter]
-            letters=letters+letter_asci
-        words_asci.append(letters)
-    return ' '.join(words_asci)
 
 print(decodeMorse('   .   . '))
-        
-        
-        
-    # ToDo: Accept dots, dashes and spaces, return human-readable message
-    #return morse_code.replace('.', MORSE_CODE['.']).replace('-', MORSE_CODE['-']).replace(' ', '')
 
->>>>>>> master
+# ToDo: Accept dots, dashes and spaces, return human-readable message
+# return morse_code.replace('.', MORSE_CODE['.']).replace('-', MORSE_CODE['-']).replace(' ', '')
