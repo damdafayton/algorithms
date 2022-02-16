19e072573f3b0821a4e28e187208fe51c5a3e1d2
backtofayton <yucelolcay@gmail.com>
2022-02-16 17:21:56 +0200

    Update

--- freecodecamp/title-case-a-sentence.js
+++ freecodecamp/title-case-a-sentence.js
@@ -1,13 +1,13 @@
 function titleCase(str) {
-  let strList=str.split(' ');
-  let newList=[];
-  for (let i=0; i<strList.length; i++){
-    let newWord=''
-    for(let k = 0; k<strList[i].length; k++){
-      if (k==0){
-        newWord+=strList[i][k].toUpperCase();
-      }else{
-        newWord+=strList[i][k].toLowerCase();
+  let strList = str.split(' ');
+  let newList = [];
+  for (let i = 0; i < strList.length; i++) {
+    let newWord = ''
+    for (let k = 0; k < strList[i].length; k++) {
+      if (k == 0) {
+        newWord += strList[i][k].toUpperCase();
+      } else {
+        newWord += strList[i][k].toLowerCase();
       }
     }
     newList.push(newWord);
@@ -15,4 +15,4 @@ function titleCase(str) {
   return newList.join(' ');
 }
 
-titleCase("I'm a little tea pot");
+console.log(titleCase("I'm a little tea pot"));
